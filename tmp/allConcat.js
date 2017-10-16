82ef7876fb1152b677faf98e6c0a4b5e1a904d77
Guest User <Guest@Epicodus-5B.local>
2017-10-16 13:07:06 -0700

    add front end for death calculator

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -4,12 +4,16 @@ $(document).ready(function(){
   $('#planets').submit(function(){
     event.preventDefault();
     let age = parseInt($("#planetAge").val());
-
+    let gender = $("#gender").val();
+    let location = $("#location").val();
+    let death = newCalc.lifeExpect(gender,location);
     let newCalc = new Calculator(age);
 
     $("#mer").text(newCalc.ageOnMercury());
     $("#ven").text(newCalc.ageOnVenus());
     $("#mars").text(newCalc.ageOnMars());
     $("#jup").text(newCalc.ageOnJupiter());
+
+    $("#deathDisplay").text(newCalc.deathCheck(age,location));
   });
 });
